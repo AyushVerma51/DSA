57f21f33b37b92f942acab47947df8de3fa1a8f8
Ayush Verma <ayushverma2804@gmail.com>
2026-09-19 02:46:29 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.43%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int cnt = 1;
+        int maxi=1;
+        for(int i=0;i<s.length();i++){
+            if(s[i] == s[i+1]){
+                cnt++;
+                maxi= max(maxi, cnt);
+            }else{
+                cnt=1;
+            }
+        }
+        return maxi;
+    }
+};
